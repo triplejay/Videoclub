7086570392d20506fa1d15c37e8673be8192a8cc
triplejay <43989858+triplejay@users.noreply.github.com>
2019-01-17 11:00:33 +0100

    Ejercicios Tema3

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -10,21 +10,21 @@
     Vista detalle pelicula
     <div class="row">
         <div class="col-sm-4">
-            <img src="{{$id['poster']}}" style="height:200px"/>
+            <img src="{{$id->poster}}" style="height:200px"/>
         </div>
         <div class="col-sm-8">
-            <h1>{{$id['title']}}</h1>
-            <h4>Año: {{$id['year']}}</h4>
-            <h4>Director: {{$id['director']}}</h4>
-            <p><strong>Resumen:</strong> {{$id['synopsis']}}</p>
-            @if($id['rented']==true)
+            <h1>{{$id->title}}</h1>
+            <h4>Año: {{$id->year}}</h4>
+            <h4>Director: {{$id->director}}</h4>
+            <p><strong>Resumen:</strong> {{$id->synopsis}}</p>
+            @if($id->rented==true)
                 <p><strong>Estado:</strong> Pelicula actualmente alquilada</p>
                 <a class="btn btn-danger" href="#">Devolver Pelicula</a>
             @else
                 <p><strong>Estado:</strong> Pelicula disponible</p>
                 <a class="btn btn-success" href="#">Alquilar Pelicula</a>
             @endif
-            <a class="btn btn-warning" href="{{ url('/catalog/edit/' . $idPelicula ) }}">Editar pelicula</a>
+            <a class="btn btn-warning" href="{{ url('/catalog/edit/' . $id->id ) }}">Editar pelicula</a>
             <a class="btn btn-light" href="{{ url('/catalog' ) }}">Volver al listado</a>
         </div>
     </div>
